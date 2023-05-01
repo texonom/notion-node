b327452c7c9d49aaaf9b3b08adf57daabf7e847d
seonglae <sungle3737@gmail.com>
2023-05-01 16:48:36 +0900

    refactor: remove examples for simple maintanence

--- examples/minimal/lib/config.ts
+++
@@ -1,5 +0,0 @@
-// TODO: change this to the notion ID of the page you want to test
-export const rootNotionPageId = '067dd719a912471ea9a3ac10710e7fdf'
-
-export const isDev =
-  process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
