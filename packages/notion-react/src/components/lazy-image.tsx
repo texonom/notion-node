3dfda578cd282606da75ce8a243a2fe5d94dbee2
seonglae <sungle3737@gmail.com>
2023-05-08 13:52:09 +0900

    feat: turbo cache & composite include tsconfig 1.1.0

--- packages/notion-react/src/components/lazy-image.tsx
+++ packages/notion-react/src/components/lazy-image.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react'
 
 import { normalizeUrl } from '@3bases/notion-utils'
-import { ImageState, LazyImageFull } from 'react-lazy-images'
 
 import { useNotionContext } from '../context'
-import { cs } from '../utils'
 
 /**
  * Progressive, lazy images modeled after Medium's LQIP technique.
@@ -27,91 +25,36 @@ export const LazyImage: React.FC<{
 
   const onLoad = React.useCallback(
     (e: any) => {
-      if (zoomable && (e.target.src || e.target.srcset))
-        if (zoomRef.current) {
-          ;(zoomRef.current as any).attach(e.target)
-        }
+      if (zoomable && (e.target.src || e.target.srcset)) if (zoomRef.current) (zoomRef.current as any).attach(e.target)
     },
     [zoomRef, zoomable]
   )
 
   const attachZoom = React.useCallback(
     (image: any) => {
-      if (zoomRef.current && image) {
-        ;(zoomRef.current as any).attach(image)
-      }
+      if (zoomRef.current && image) (zoomRef.current as any).attach(image)
     },
     [zoomRef]
   )
 
   const attachZoomRef = React.useMemo(() => (zoomable ? attachZoom : undefined), [zoomable, attachZoom])
 
-  if (previewImage) {
-    const aspectRatio = previewImage.originalHeight / previewImage.originalWidth
-
-    if (components.Image)
-      // TODO: could try using next/image onLoadComplete to replace LazyImageFull
-      // while retaining our blur implementation
-      return (
-        <components.Image
-          src={src}
-          alt={alt}
-          style={style}
-          className={className}
-          width={previewImage.originalWidth}
-          height={previewImage.originalHeight}
-          blurDataURL={previewImage.dataURIBase64}
-          placeholder='blur'
-          priority={priority}
-          onLoad={onLoad}
-        />
-      )
-
+  if (previewImage && components.Image) {
+    // TODO: could try using next/image onLoadComplete to replace LazyImageFull
+    // while retaining our blur implementation
     return (
-      <LazyImageFull src={src} {...rest} experimentalDecode={true}>
-        {({ imageState, ref }) => {
-          const isLoaded = imageState === ImageState.LoadSuccess
-          const wrapperStyle: React.CSSProperties = {
-            width: '100%'
-          }
-          const imgStyle: React.CSSProperties = {}
-
-          if (height) {
-            wrapperStyle.height = height
-          } else {
-            imgStyle.position = 'absolute'
-            wrapperStyle.paddingBottom = `${aspectRatio * 100}%`
-          }
-
-          return (
-            <div className={cs('lazy-image-wrapper', isLoaded && 'lazy-image-loaded', className)} style={wrapperStyle}>
-              <img
-                className='lazy-image-preview'
-                src={previewImage.dataURIBase64}
-                alt={alt}
-                ref={ref}
-                style={style}
-                decoding='async'
-              />
-
-              <img
-                className='lazy-image-real'
-                src={src}
-                alt={alt}
-                ref={attachZoomRef}
-                style={{
-                  ...style,
-                  ...imgStyle
-                }}
-                width={previewImage.originalWidth}
-                height={previewImage.originalHeight}
-                decoding='async'
-                loading='lazy'
-              />
-            </div>
-          )
-        }}
-      </LazyImageFull>
+      <components.Image
+        src={src}
+        alt={alt}
+        style={style}
+        className={className}
+        width={previewImage.originalWidth}
+        height={previewImage.originalHeight}
+        blurDataURL={previewImage.dataURIBase64}
+        placeholder='blur'
+        priority={priority}
+        onLoad={onLoad}
+      />
     )
   } else {
     // TODO: GracefulImage doesn't seem to support refs, but we'd like to prevent
