21cf0af7b377c06d906886326dd1e8c41b7acc95
seonglae <sungle3737@gmail.com>
2024-11-28 12:23:30 +0000

    fix(cli): remove hierarchy folder from export

--- packages/cli/src/notion/index.ts
+++ packages/cli/src/notion/index.ts
@@ -1,5 +1,8 @@
 import { Command, Option } from 'clipanion'
 
+import { getCanonicalPageId } from '@texonom/nutils'
+import type { ExtendedRecordMap } from '@texonom/ntypes'
+
 export abstract class NotionCommand extends Command {
   // for private page or eoi like github inline block
   token = Option.String('-t,--token', {
@@ -7,3 +10,6 @@ export abstract class NotionCommand extends Command {
   })
   async execute() {}
 }
+
+export const getBlockLink = (blockId: string, recordMap: ExtendedRecordMap, domain = 'https://texonom.com') =>
+  `${domain}/${getCanonicalPageId(blockId, recordMap)}`
