3b20cf16eff59755c496a5c0ae982a25060719ba
Seonglae Cho <sungle3737@gmail.com>
2025-06-09 02:06:51 +0100

    refactor: switch packages to vite

---
+++ packages/cli/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import pkg from './package.json' assert { type: 'json' }
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/main.ts',
+      formats: ['es'],
+      fileName: () => 'main.js'
+    },
+    outDir: 'build/src',
+    target: 'node16',
+    sourcemap: true,
+    minify: true,
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {}),
+        'fs',
+        'fs/promises',
+        'path',
+        'stream',
+        'util'
+      ]
+    }
+  },
+  plugins: [dts({ tsconfigPath: 'tsconfig.json', outDir: 'build/src' })]
+})
+
