a1d40f629daad91bb7a5e30f70168c9b3a161dc5
Seonglae Cho <sungle3737@gmail.com>
2025-06-09 02:06:51 +0100

    refactor: switch packages to vite

---
+++ packages/nclient/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import pkg from './package.json' assert { type: 'json' }
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: () => 'index.js'
+    },
+    outDir: 'build',
+    target: 'es2015',
+    sourcemap: true,
+    minify: true,
+    rollupOptions: {
+      external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})]
+    }
+  },
+  plugins: [dts({ tsconfigPath: 'tsconfig.json', outDir: 'build' })]
+})
